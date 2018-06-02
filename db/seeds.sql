e7e180243544d8fa134d9347803251c552d2595e
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-06-01 19:42:14 -0500

    main setup and functionality

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetarian', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+
+
